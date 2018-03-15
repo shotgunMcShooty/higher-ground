6a86e63ce3ca705bfa5079a1a53c873fc70f196c
Leonardo <leo@master>
2018-03-15 17:02:53 +0100

    Se completa la clase Artista con atributos y metodos

--- 3EVA/src/ejercicio4_museo/Artista.java
+++ 3EVA/src/ejercicio4_museo/Artista.java
@@ -1,5 +1,60 @@
 package ejercicio4_museo;
 
-public class Artista {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class Artista {
+	private String nombre, lugarNac;
+	private Date nacimiento, muerte;
+	
+	public Artista() {
+	}
+
+	public Artista(String nombre, String lugarNac, Date nacimiento, Date muerte) {
+		super();
+		this.nombre = nombre;
+		this.lugarNac = lugarNac;
+		this.nacimiento = nacimiento;
+		this.muerte = muerte;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getLugarNac() {
+		return lugarNac;
+	}
+
+	public void setLugarNac(String lugarNac) {
+		this.lugarNac = lugarNac;
+	}
+
+	public Date getNacimiento() {
+		return nacimiento;
+	}
+
+	public void setNacimiento(Date nacimiento) {
+		this.nacimiento = nacimiento;
+	}
+
+	public Date getMuerte() {
+		return muerte;
+	}
+
+	public void setMuerte(Date muerte) {
+		this.muerte = muerte;
+	}
+	
+	public String toString() {
+		SimpleDateFormat fecha = new SimpleDateFormat("dd/mm/yyyy");
+		String nacimiento = fecha.format(this.nacimiento);
+		String muerte = fecha.format(this.muerte);
+		return "Nombre: "+nombre+"\nLugar de nacimiento: "+lugarNac+"\nNacimiento: "+nacimiento+
+				"Fallecimiento: "+muerte;
+	}
 }
