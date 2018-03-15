90a508f383ee6690c75368a380b078e3df8440e7
Leonardo <leo@master>
2018-03-16 00:27:58 +0100

    Se ha creado la documentación javadoc en Artista y Obra

--- 3EVA/src/ejercicio4_museo/Artista.java
+++ 3EVA/src/ejercicio4_museo/Artista.java
@@ -4,24 +4,50 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
- * @author leona
+ * Clase Artista, sirve para crear Artistas de cuadros y modificar sus datos.
+ * @author Leonardo
+ * @version 15-03-2018
+ * @see Date
  *
  */
 public class Artista {
-	private String nombre, lugarNac;
-	private Date nacimiento, muerte;
-	
+	/**
+	 * Atributo nombre del artista.
+	 */
+	private String nombre;
+	/**
+	 * Atributo lugar de nacimiento del artista.
+	 */
+	private String lugarNac;
+	/**
+	 * Atributo fecha de nacimiento del artista.
+	 */
+	private Date nacimiento;
+	/**
+	 * Atributo fecha de defuncion del artista.
+	 */
+	private Date muerte;
+	/**
+	 * Constructor por defecto.
+	 */
 	public Artista() {
 	}
-
+	/**
+	 * Contructor con 4 parámetros.
+	 * Crea un artista con todos los datos.
+	 * @param nombre Nombre del artista.
+	 * @param lugarNac Lugar de nacimiento del artista.
+	 * @param nacimiento Fecha de nacimiento.
+	 * @param muerte Fecha de defunción.
+	 */
 	public Artista(String nombre, String lugarNac, Date nacimiento, Date muerte) {
-		super();
 		this.nombre = nombre;
 		this.lugarNac = lugarNac;
 		this.nacimiento = nacimiento;
 		this.muerte = muerte;
 	}
-
+	
+	//Metodos setters y getters
 	public String getNombre() {
 		return nombre;
 	}
@@ -53,7 +79,15 @@ public class Artista {
 	public void setMuerte(Date muerte) {
 		this.muerte = muerte;
 	}
-	
+	/**
+	 * Método equals. Compara que dos objetos sean iguales.
+	 * @param o Objeto que recibe, puede ser de cualquier tipo.
+	 * @return <ul>
+	 * 				<li><strong>true:</strong> Los atributos del objeto que se recibe tienen los mismos valores que los del objeto con el se compara.</li>
+	 * 				<li><strong>false:</strong> El objeto no es del tipo Artista, no indica ninguna posicion de memoria o tiene valores distintos.</li>
+	 * 			</ul>
+	 * @see Object
+	 */
 	public boolean equals(Object o) {
 		if(o == null) return false;
 		if(o == this ) return true;
@@ -66,11 +100,20 @@ public class Artista {
 		}else
 			return false;
 	}
-	
+	/**
+	 * Método que llama a toString
+	 * @return La cadena de String generado en el método toString()
+	 */
 	public String Muestra() {
 		return toString();
 	}
 	
+	/**
+	 * Método que imprime todos los datos del artista
+	 * @return Los datos del artista
+	 * @see DateFormat
+	 * @see SimpleDateFormat
+	 */
 	public String toString() {
 		SimpleDateFormat fecha = new SimpleDateFormat("dd/mm/yyyy");
 		String nacimiento = fecha.format(this.nacimiento);
--- 3EVA/src/ejercicio4_museo/Obra.java
+++ 3EVA/src/ejercicio4_museo/Obra.java
@@ -1,35 +1,83 @@
 package ejercicio4_museo;
-
+/**
+ * Clase Obra,<p>
+ * es una clase abstracta que sirve de plantilla para sus clases hijas.
+ * @author Leonardo
+ *@see ejercicio4_museo#Artista
+ *@see ejercicio4_museo#Pintura
+ *@see ejercicio4_museo#Escultura
+ */
 public abstract class Obra {
+	/**
+	 * Atributo Nombre de la obra.
+	 */
 	protected String titulo;
+	/**
+	 * Número de identificación de la obra.
+	 */
 	protected long nInventario;
+	/**
+	 * Artista que creó la obra.
+	 * @see ejercicio4_museo#Artista
+	 */
 	protected Artista autor;
+	/**
+	 * Año en el que se realizó la obra.
+	 */
 	protected int anio;
 	
-	
+	/**
+	 * Constructor con tres parámetros. Crea una pieza de museo.<p>
+	 * <strong>No</strong> se pide el autor.
+	 * @param titulo Título de la obra.
+	 * @param nInventario Número de identificación de la obra.
+	 * @param anio Año de realización de la obra.
+	 */
 	public Obra(String titulo, long nInventario, int anio) {
 		this.titulo = titulo;
 		this.nInventario = nInventario;
 		this.anio = anio;
 	}
 
-
+	/**
+	 * Constructor con cuatro parámetros. Crea una pieza de museo.<p>
+	 * Es necesario indicar el autor.
+	 * @param titulo Título de la obra.
+	 * @param nInventario Número de identificación de la obra.
+	 * @param autor Artista que realizó la obra.
+	 * @param anio Año de realización de la obra.
+	 * @see ejercicio4_museo#Artista
+	 */
 	public Obra(String titulo, long nInventario, Artista autor, int anio) {
 		this.titulo = titulo;
 		this.nInventario = nInventario;
 		this.autor = autor;
 		this.anio = anio;
 	}
-	
+	/**
+	 * Permite vincular (o reemplazar) un autor a una obra ya creada.
+	 * @param autor Nuevo autor de la obra.
+	 */
 	public void setAutor(Artista autor) {
 		this.autor=autor;
 	}
-	
+	/**
+	 * Método que imprime los datos de la obra
+	 * @return Todos los datos de la obra.
+	 */
 	public String Muestra() {
 		return "Título: "+titulo+"\nNumero de inventario: "+nInventario+"\nAño: "+anio+
 				"\nAutor:\n"+autor;
 	}
-	
+	/**
+	 * Método equals. Compara que dos objetos sean iguales.
+	 * @param o Objeto que recibe, puede ser de cualquier tipo.
+	 * @return <ul>
+	 * 				<li><strong>true:</strong> Los atributos del objeto que se recibe tienen los mismos valores que los del objeto con el se compara.</li>
+	 * 				<li><strong>false:</strong> El objeto no es del tipo Artista, no indica ninguna posicion de memoria o tiene valores distintos.</li>
+	 * 			</ul>
+	 * @see Object
+	 */
 	public boolean equals(Object o) {
 		if(o == null) return false;
 		if(o == this ) return true;
@@ -42,7 +90,10 @@ public abstract class Obra {
 		}else
 			return false;
 	}
-	
+	/**
+	 * Método que llama a muestra()
+	 * @return La cadena de texto generada en el método muestra()
+	 */
 	public String toString() {
 		return Muestra();
 	}
